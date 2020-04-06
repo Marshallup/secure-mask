7e449ad962986e7fad5702dd90b31275b014261b
Mikhail Teslya <televonvea@gmail.com>
2020-04-06 17:38:12 +0700

    With out forms

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -22,9 +22,44 @@ $(document).ready(function() {
             }, 900)
         }
     })
+    // .hasClass("menu-on"))
+    $('button').click(function(event) {
 
-    $('.slider__block').slick({
-        dots: true,
-        speed: 1000
-    });
+        if (event.target.classList.contains('button__form')) {
+            return
+        } else if ( event.target.classList.contains('button__slider')) {
+            return
+        } else if (event.target.classList.contains('button__contacts') ) {
+            return
+        } else {
+            $('html, body').animate({
+                scrollTop: $('#order').offset().top
+            }, 1000)
+        }
+    })
+
+    // $('.slider__block').slick({
+    //     settings: "unslick",
+    //     dots: true,
+    //     speed: 1000
+    // });
+    // $('.slider__block').slick("unslick")
 })
+let menu = document.querySelector('.main__menu');
+let burger = document.querySelector('.main__burger');
+let body = document.body;
+window.onload = () => {
+    // let burger = document.querySelector('.main__burger')
+    burger.onclick = (e) => {
+        burger.classList.toggle('burger__active')
+        menu.classList.toggle('menu__active')
+        body.classList.toggle('lock')
+        for (let link of menu.children ) {
+            link.onclick = () => {
+                burger.classList.remove('burger__active')
+                body.classList.remove('lock')
+                menu.classList.remove('menu__active')
+            }
+        }
+    }
+}
